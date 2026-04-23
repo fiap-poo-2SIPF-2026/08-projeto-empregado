3382285206fc6def6dc027603d8c791e4bce4524
Laboratório FIAP <marcos.selmini@uol.com.br>
2026-04-23 09:53:16 -0300

    classes do projeto

---
+++ src/Empregado.java
@@ -0,0 +1,15 @@
+public class Empregado {
+    long matricula;
+    String nome;
+
+    public Empregado(long matricula, String nome) {
+        super();
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    public double calcularSalario() {
+        return 0;
+    }
+
+}
